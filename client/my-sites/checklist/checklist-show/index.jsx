091038e95e6ee3b3d098cab3f4f5a30c7670102c
Mark Ryall <mark@ryall.name>
2017-12-14 16:44:23 +1100

    guided tour for user avatar

--- client/my-sites/checklist/checklist-show/index.jsx
+++ client/my-sites/checklist/checklist-show/index.jsx
@@ -42,11 +42,11 @@ class ChecklistShow extends PureComponent {
 				status,
 			} );
 
-			if ( url ) {
-				page( url );
-			}
 			if ( tour ) {
 				requestTour( tour );
+			}
+			if ( url ) {
+				page( url );
 			}
 		}
 	};
